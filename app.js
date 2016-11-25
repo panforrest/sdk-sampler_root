a1afa7b74765e66e6870f8f557552a873acbc1a4
panforrest <guoqianp@gmail.com>
2016-11-24 21:20:46 -0500

    12_yelp template

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var twitter = require('./routes/twitter');
+var yelp = require('./routes/yelp');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
+app.set('view engine', 'mustache');
+app.engine('mustache', require('hogan-middleware').__express)
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,7 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/twitter', twitter)
+app.use('/twitter', twitter);
+app.use('/yelp', yelp)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
